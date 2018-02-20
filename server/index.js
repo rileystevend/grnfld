5bf35ca9ae7cb630d5bb3f783af5932957b8ef37
Andrew Dibble <acdibble@gmail.com>
2018-02-20 15:51:06 -0600

    Sitedev (#5)
    * Add port for deployment
    
    * Implement basic routing

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/../node_modules'));
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('listening on port 3000!');
 });
 
