c4c88440b0511aff4f2bcc032c02bf5d108c6cd5
Hack Reactor Student <student@hackreactor.com>
2018-02-20 14:30:20 -0600

    Add database file

--- database-pg/index.js
+++ database-pg/index.js
@@ -1,10 +1,12 @@
-var knex = require('knex')({
+const config = require('./config');
+
+const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: 'FILL_ME_IN',
-    user: 'FILL_ME_IN',
-    password: 'FILL_ME_IN',
-    database: 'FILL_ME_IN'
+    host: config.HOST,
+    user: config.USER,
+    password: config.PASSWORD,
+    database: config.DATABASE
   }
 });
 
