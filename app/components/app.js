69ba2a38d0329346dd580c20b52be01462accc9f
Andrew Dibble <acdibble@gmail.com>
2018-02-21 15:57:33 -0600

    Add 'otherwise' redirect to router

--- app/components/app.js
+++ app/components/app.js
@@ -4,20 +4,23 @@ angular.module('app')
 .component('app', {
   bindings: {},
   controller: 'AppCtrl',
-  templateUrl: '/templates/app.html'
+  templateUrl: 'templates/app.html'
 })
 .config(function($routeProvider) {
   $routeProvider
   .when('/', {
-    templateUrl: '../templates/main.html',
+    templateUrl: 'templates/main.html',
     controller: 'AppCtrl'
   })
   .when('/submit', {
-    templateUrl: '../templates/submit.html',
+    templateUrl: 'templates/submit.html',
     controller: 'SubmitCtrl'
   })
   .when('/login', {
-    templateUrl: '../templates/login.html',
+    templateUrl: 'templates/login.html',
     controller: 'AppCtrl'
-  });
+  })
+  .otherwise({
+    redirectTo: '/'
+  })
 });
