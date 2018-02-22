06787e22125574d1efd58a327b4ccaabe7e06b9a
Hack Reactor Student <student@hackreactor.com>
2018-02-22 13:35:29 -0600

    renamed posts-first-page to posts-list throughout and deleted template list files

--- app/components/postsFrontPage.js
+++ app/components/postsFrontPage.js
@@ -1,17 +1,9 @@
 angular.module('app')
-    .controller('PostsCtrl', function () {
-      
-    })
     .directive('postsFrontPage', function() {
 
       return {
         restrict: 'E',
-        templateUrl: 'templates/posts-front-page.html',
-        // controller: function(itemsService) {
-        //   itemsService.getTop10( (data) => {
-        //     this.posts = [{title: 'HERES THE FIRST POST'}]/*data*/;
-        //   });
+        templateUrl: 'templates/posts-list.html',
         }
-        controllerAs: postsFrontPage
       
 })
