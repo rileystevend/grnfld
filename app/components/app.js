c88d9fd4ab84830a7645a2dcdb08376c6880d53e
Steven Riley <rileystevend@gmail.com>
2018-02-28 17:09:44 -0600

    refactored from appCtrl to MainCtrl

--- app/components/app.js
+++ app/components/app.js
@@ -1,23 +1,6 @@
 angular.module('app')
 
 .controller('AppCtrl', function (postsService, $rootScope) {
-
-  // this.posts = [{title: 'Heres the first post'}, {title: 'Heres the SECOND post'}];
-  // this.currentPost = this.posts[0];
-  this.comments = [{text: 'hey first comment!'}, {text: 'hey second comment!'}]
-  postsService.getAll(data => {
-    console.log(data);
-    this.posts = data;
-  });
-
-  this.handlePostClick = (clickedvalue) => {
-    this.currentPost = this.posts[clickedvalue];
-    postsService.getComments(this.currentPost.post_id, (data) => {
-      console.log(data);
-      this.comments = data;
-    })
-  };
-
   $rootScope.userId = 0;
 })
 .component('app', {
@@ -29,7 +12,7 @@ angular.module('app')
   $routeProvider
     .when('/', {
       templateUrl: 'templates/main.html',
-      controller: 'AppCtrl'
+      controller: 'MainCtrl'
     })
     .when('/submit', {
       templateUrl: 'templates/submit.html',
--- app/components/main.js
+++ app/components/main.js
@@ -1,2 +1,16 @@
 angular.module('app')
-.controller('MainCtrl', function (){})
+.controller('MainCtrl', function (postsService, $rootScope){
+	this.comments = [{text: 'hey first comment!'}, {text: 'hey second comment!'}]
+  	postsService.getAll(data => {
+      console.log(data);
+      this.posts = data;
+  	});
+
+  this.handlePostClick = (clickedvalue) => {
+    this.currentPost = this.posts[clickedvalue];
+    postsService.getComments(this.currentPost.post_id, (data) => {
+      console.log(data);
+      this.comments = data;
+    })
+  };
+})
