c2b57e7356b1da9a52fc35360fc2b7c53e6ed667
acdibble <acdibble@gmail.com>
2018-02-28 20:02:20 -0600

    Rebase

--- app/components/login.js
+++ app/components/login.js
@@ -1,21 +1,11 @@
 angular.module('app')
-<<<<<<< 9ef3aaf374fcdf68268310d0a46d34a380c353fd
 .controller('LoginCtrl', function($scope, usersService, $rootScope, $location) {
-=======
-.controller('LoginCtrl', function($scope, usersService, $cookies, $rootScope) {
->>>>>>> Add user id to root scope
   $scope.username = '';
   $scope.password = '';
   $scope.submit = function() {
     usersService.login($scope.username, $scope.password, res => {
-<<<<<<< 9ef3aaf374fcdf68268310d0a46d34a380c353fd
       $rootScope.userId = res.data.user_id;
       $location.path('/');
-=======
-      console.log($cookies.getAll())
-      console.log(res);
-      $rootScope.userId = res.data.user_id;
->>>>>>> Add user id to root scope
     });
   };
 });
